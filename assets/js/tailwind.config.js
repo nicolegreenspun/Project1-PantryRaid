549a01a06ae1c516619150b16f5bf85fc93c0fb5
Nicole Greenspun <greenspun.nicole@gmail.com>
2024-03-21 18:52:55 -0400

    Updated index.html and details.html with tailwindcss

---
+++ assets/js/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./assets./js/**/*.js", "./layouts/**/*.html", "./content/**/*.md"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
